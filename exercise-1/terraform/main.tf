b61aa7fc203966c4423b0a2605d7bb42ee5595fe
benkorichard <benko.richard86@gmail.com>
2026-09-26 10:50:02 +0200

    test

--- exercise-1/terraform/main.tf
+++ exercise-1/terraform/main.tf
@@ -1,15 +1,10 @@
 module "vpc" {
+	for_each = local.vpcs
+
 	source = "git::https://github.com/benkorichard/terraform-aws-vpc?ref=feat/init"
 
-    vpc_cidr = "10.0.0.0/16"
-	name     = "exercise-1"
-
-	public_subnet_cidrs = {
-		"eu-central-1a" = "10.0.1.0/24"
-		"eu-central-1b" = "10.0.2.0/24"
-	}
-	private_subnet_cidrs = {
-		"eu-central-1a" = "10.0.11.0/24"
-		"eu-central-1b" = "10.0.12.0/24"
-	}
+	vpc_cidr             = each.value.vpc_cidr
+	name                 = each.value.name
+	public_subnet_cidrs  = each.value.public_subnet_cidrs
+	private_subnet_cidrs = each.value.private_subnet_cidrs
 }
